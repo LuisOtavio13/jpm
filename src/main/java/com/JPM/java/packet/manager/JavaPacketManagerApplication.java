04d13b713350b983693ee0161116b557563d9716
LuisOtavio13 <roberdoogarcia@gmail.com>
2026-03-21 23:16:13 -0300

    init project

---
+++ src/main/java/com/JPM/java/packet/manager/JavaPacketManagerApplication.java
@@ -0,0 +1,13 @@
+package com.JPM.java.packet.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPacketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPacketManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JPM/java/packet/manager/JavaPacketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JPM.java.packet.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPacketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
